9d60b8c5922438a0ff3161af7720e86a569e1273
karamTahhan <kata07001@utb.vaxjo.se>
2024-12-13 08:30:23 +0100

    Add project files.

---
+++ loopar_2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace loopar_2
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("skriv ett heltal");
+            string str=Console.ReadLine();
+            int tal= int.Parse(str);
+            while(tal!=0)
+            {
+                Console.WriteLine("skriv igen (skriv in 0 för att avsluta");
+                str = Console.ReadLine();
+                tal = int.Parse(str);
+
+
+            }
+            Console.WriteLine("du har avslutat programmet");
+
+
+            
+            
+
+        }
+
+    }
+
+}
